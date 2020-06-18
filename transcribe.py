f1210b81bb5b629a97169f650d9a10608a93dd7a
Chris Proctor <chris.proctor@gmail.com>
2020-06-18 11:10:25 -0400

    Adding gitignore, requirements

--- transcribe.py
+++ transcribe.py
@@ -1,20 +1,12 @@
 #! /usr/local/bin/python3
 
 import boto3
-from botocore.exceptions import ClientError
 from argparse import ArgumentParser
 from subprocess import run
 from pathlib import Path
 import logging
 import json
 import sys
-
-# - Convert file if necessary
-# - push to AWS bucket
-# - issue trascribe command
-# - check status
-
-# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transcribe.html#TranscribeService.Client.start_transcription_job
 
 LANGUAGE_CODES = ['en-US', 'es-US', 'en-AU', 'fr-CA', 'en-GB', 'de-DE', 'pt-BR', 'fr-FR', 'it-IT', 'ko-KR', 'es-ES', 'en-IN', 'hi-IN', 'ar-SA', 'ru-RU', 'zh-CN', 'nl-NL', 'id-ID', 'ta-IN', 'fa-IR', 'en-IE', 'en-AB', 'en-WL', 'pt-PT', 'te-IN', 'tr-TR', 'de-CH', 'he-IL', 'ms-MY', 'ja-JP', 'ar-AE']
 
